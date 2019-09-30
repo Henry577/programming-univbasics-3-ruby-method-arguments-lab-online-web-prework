7cdbf0cfde861a6bb607bb0fcdb96ab3abc207a3
Henry Allan <henryallan50@gmail.com>
2019-09-30 16:15:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,6 @@
 
 name = "henry"
 
-language = "Ruby"
 def introduction(name)
   puts "Hi, my name is #{name}"
 end
@@ -14,7 +13,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and i am learning to program in #{language}"
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name}"
   
   if language == "Ruby"
